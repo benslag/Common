fe08365c3f89f35521a49db391ae02c5f1faeed9
Ben Slaghekke <ben.slaghekke@gmail.com>
2024-12-20 12:23:46 +0100

    Initial after moving to Common

---
+++ lib/debug/debug.cpp
@@ -0,0 +1,100 @@
+//
+// debug.cpp - a simple debugger
+//
+// Note that if _Debug is 0, the arguments of LOG are NOT evaluated.
+//   e.g. LOG ("The value of i = %d\n", i++);
+//   If _LOG is defined as 0, i is NOT increased.
+//
+// BSla, 10 october 2023
+//       31 october 2024 Add multi-level, multi output channel
+
+#include <stdarg.h>
+#include <stdio.h>
+#include "debug.h"
+#include <string.h>
+
+static const int N_PFS = 10;
+static const int LOG_P_BUF_SIZE = 200;
+
+static DebugPrintFunction debugPrintFunctions[N_PFS];
+static int pfIndex = 0; // first EMPTY entry in debugPrintFunctions
+
+static int debugCurrentLevel = 10; // log everything
+
+void DebugSetLevel(int level)
+{
+   if (level >= 0 && level <= 10)
+      debugCurrentLevel = level;
+
+   //  loglevels are from 0..10:
+   //  0 = ERROR
+   //  1 = WARNING
+   //  2..10 = LOG, decreasing level of importance
+}
+
+static void myPrint(const char *buffer)
+{
+   for (int i = 0; i < pfIndex; i++)
+   {
+      debugPrintFunctions[i](buffer);
+   }
+}
+
+bool DebugAddPrintFunction(DebugPrintFunction pf)
+{
+   bool r = false;
+
+   if (pfIndex < N_PFS)
+   {
+      debugPrintFunctions[pfIndex++] = pf;
+      r = true;
+   }
+   return r;
+}
+
+bool DebugRmPrintFunction(DebugPrintFunction pf)
+// remove the first added print function pf
+{
+   bool r = false;
+   for (int i = 0; i < pfIndex; i++)
+   {
+      if (debugPrintFunctions[i] == pf)
+      {
+         r = true;
+         for (int j = i; j < pfIndex - 1; j++)
+         {
+            debugPrintFunctions[j] = debugPrintFunctions[j + 1];
+         }
+         --pfIndex;
+         break;
+      }
+   }
+   return r;
+}
+
+void DebugReal(const int level, const char *fmt, ...)
+{
+   if (level <= debugCurrentLevel)
+   {
+      va_list args;
+      char buffer[LOG_P_BUF_SIZE];
+      buffer[0] = '\0';
+      if (level == 0)
+         strcpy(buffer, "ERROR: ");
+      else if (level == 1)
+         strcpy(buffer, "WARNING: ");
+
+      char *bp = &buffer[strlen(buffer)];
+      va_start(args, fmt);
+      vsnprintf(bp, LOG_P_BUF_SIZE, fmt, args);
+      va_end(args);
+      myPrint(buffer);
+   }
+}
+
+const char *toCCP(bool b)
+{
+   static const char *True = "true";
+   static const char *False = "false";
+   return b ? True : False;
+}
---
+++ lib/debug/debug.h
@@ -0,0 +1,61 @@
+//
+// debug.h - a simple debugger
+//
+// The following macros exist:
+//
+// DebugAddPrintFunction (PrintFunction x) 
+//                     adds a log print function (need at least 1)
+// LOG     (fmt, ...)  logs a message using a printf style format
+// WARNING (fmt, ...)  logs a warning message, ...
+// ERROR   (fmt, ...)  logs an error message, ...
+//
+// Note that if _D is 0, the arguments of any of the macros are NOT evaluated.
+//   e.g. LOG ("The value of i = %d\n", i++);
+//   If _LOG is defined as 0, i is NOT increased.
+//
+// Macro toCCP (x) can be used to print the value of a boolean.
+//   e.g. LOG ("Boolean x = %s\n", toCCP (x));
+//   prints either "Boolean x = true" or "Boolean x = false"
+//   (note: CCP is const char ptr)
+// 
+// BSla, 10 october 2023
+//       31 october 2024 Add multi-level, multi output channel. Decouple from Serial.print
+//
+#ifndef __DEBUG_H
+#define __DEBUG_H
+
+#ifndef _DEBUG
+#define _DEBUG 1
+#endif
+
+typedef void (*DebugPrintFunction) (const char *buffer);
+
+extern bool  DebugAddPrintFunction (DebugPrintFunction pf);
+extern void  DebugSetLevel (int level);
+extern void  DebugReal (const int level, const char *fmt, ...);
+extern const char* toCCP (bool b);
+
+
+
+#define DEBUGSetLevel(x)         DebugSetLevel (x)
+#define DEBUGAddPrintFunction(x) DebugAddPrintFunction (x)
+#define DEBUGRmPrintFunction(x)  DebugRmPrintFunction (x)
+#define WARNING(...) DebugReal (1,__VA_ARGS__)
+#define ERROR(...)   DebugReal (0,__VA_ARGS__)
+
+#if _DEBUG == 1
+#define DEBUG(...) DebugReal (10,__VA_ARGS__)
+#define LOG(...)   DebugReal (8,__VA_ARGS__)
+
+
+#define DEBUGAddPrintFunction(x) DebugAddPrintFunction (x)
+#define DEBUGRmPrintFunction(x)  DebugRmPrintFunction (x)
+
+#elif _DEBUG == 0
+#define DEBUG(...) {}
+#define LOG(...) {}
+#else
+#error Debug.h: _DEBUG is not defined as 0 or 1
+#endif  // if _DEBUG==1
+
+#endif  // __DEBUG_H
